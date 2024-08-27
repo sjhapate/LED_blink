c20005b37b325b71ab3154b0ab131c6fbde0c028
sjhapate <hardipec009@gmail.com>
2024-08-27 21:18:24 +0200

    Changed stack size and configMAX_PRIORITIES

--- main/main.c
+++ main/main.c
@@ -12,11 +12,14 @@
 #include "LED.h"                   // Include the LED header file for LED control functions
 #include "ADC.h"                   // Include the ADC header file for ADC reading functions
 
+#define STACK_SIZE_TASK1 (4096)
+#define STACK_SIZE_TASK2 (2048)
+
 // Main application entry point
 void app_main(void)
 {
     // Create a FreeRTOS task for LED flashing
-    xTaskCreate(led_flasher, "led_flasher", 4096, NULL, 1, NULL);
+    xTaskCreate(led_flasher, "led_flasher", STACK_SIZE_TASK1, NULL, 4, NULL);
     // Create a FreeRTOS task for reading ADC values
-    xTaskCreate(adc_read, "adc_read", 4096, NULL, 1, NULL);
+    xTaskCreate(adc_read, "adc_read", STACK_SIZE_TASK2, NULL, 4, NULL);
 }
